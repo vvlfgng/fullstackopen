5cbb7d8c866f035758ab995faee27f28247871d8
vvlfgng <me@vvlfgng.com>
2023-06-13 11:46:59 -0400

    upload: 1.4 course information step4

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -25,33 +25,36 @@ const Content = (props) => {
 }
 
 const Total = (props) => {
+  const total = props.total.reduce((sum, part) => sum + part.exercises, 0)
   return (
     <>
-    <p>Number of exercises {props.total}</p>
+    <p>Number of exercises {total}</p>
     </>
   )
 }
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={[part1, part2, part3]} />
-      <Total total={part1.exercises + part2.exercises + part3.exercises} />
+      <Content parts={parts} />
+      <Total total={parts} />
     </div>
   )
 }
