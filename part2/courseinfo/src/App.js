9270082160d69bc299af06c7f811de1637154ffe
vvlfgng <me@vvlfgng.com>
2023-06-20 11:59:26 -0400

    upload: 2.1 Course information step6

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -16,11 +16,7 @@ const Part = (props) => {
 
 const Content = (props) => {
   return (
-    <>
-    <Part part={props.parts[0]} />
-    <Part part={props.parts[1]} />
-    <Part part={props.parts[2]} />
-    </>
+    props.parts.map(part => <Part part={part} key={part.id} />)
   )
 }
 
@@ -33,32 +29,40 @@ const Total = (props) => {
   )
 }
 
+const Course = (props) => {
+  return (
+    <>
+    <Header course={props.course.name} />
+    <Content parts={props.course.parts} />
+    <Total total={props.course.parts} />
+    </>
+  )
+}
+
 const App = () => {
   const course = {
+    id: 1,
     name: 'Half Stack application development',
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10
+        exercises: 10,
+        id: 1
       },
       {
         name: 'Using props to pass data',
-        exercises: 7
+        exercises: 7,
+        id: 2
       },
       {
         name: 'State of a component',
-        exercises: 14
+        exercises: 14,
+        id: 3
       }
     ]
   }
 
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total total={course.parts} />
-    </div>
-  )
+  return <Course course={course} />
 }
 
 export default App
