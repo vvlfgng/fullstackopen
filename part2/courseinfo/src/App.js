378c354c61cb907f3b93ef1313a532fdc564cd72
vvlfgng <me@vvlfgng.com>
2023-06-20 11:48:26 -0400

    initialize: part2 courseinfo

---
+++ part2/courseinfo/src/App.js
@@ -0,0 +1,64 @@
+const Header = (props) => {
+  return (
+    <>
+      <h1>{props.course}</h1>
+    </>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <>
+    <p>{props.part.name} {props.part.exercises}</p>
+    </>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <>
+    <Part part={props.parts[0]} />
+    <Part part={props.parts[1]} />
+    <Part part={props.parts[2]} />
+    </>
+  )
+}
+
+const Total = (props) => {
+  const total = props.total.reduce((sum, part) => sum + part.exercises, 0)
+  return (
+    <>
+    <p>Number of exercises {total}</p>
+    </>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total total={course.parts} />
+    </div>
+  )
+}
+
+export default App
