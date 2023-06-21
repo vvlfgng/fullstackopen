733b00e9d3faf701b082d6322f25161a995c9bcc
vvlfgng <me@vvlfgng.com>
2023-06-21 13:49:30 -0400

    upload: 2.6 The Phonebook Step1

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -6,19 +6,28 @@ const App = () => {
   ]) 
   const [newName, setNewName] = useState('')
 
+  const addPerson = (event) => {
+    event.preventDefault()
+    setPersons([...persons, { name: newName }])
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
-        <div>
-          name: <input />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
+      <form onSubmit={addPerson}>
+        <input
+          value={newName}
+          onChange={handleNameChange}
+        />
+        <button type="submit">add</button>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map(person => <div>{person.name}</div>)}
     </div>
   )
 }
