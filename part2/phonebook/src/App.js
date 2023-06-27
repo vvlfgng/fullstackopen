029de1d134ae747320bc96d862fe5720328b3f39
vvlfgng <me@vvlfgng.com>
2023-06-27 17:35:15 -0400

    upload: 2.13 The Phonebook step8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,8 +1,8 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Person from './components/Person'
+import personsService from './services/persons'
 
 const App = () => {
   const [persons, setPersons] = useState([])
@@ -11,10 +11,10 @@ const App = () => {
   const [newSearch, setNewSearch] = useState('')
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        setPersons(response.data)
+    personsService
+      .getAll()
+      .then(initialPersons => {
+        setPersons(initialPersons)
       })
   }, [])
   
@@ -37,10 +37,10 @@ const App = () => {
       number: newNumber,
     }
 
-    axios
-    .post('http://localhost:3001/persons', person)
-    .then(response => {
-      setPersons(persons.concat(response.data))
+    personsService
+    .create(person)
+    .then(newPerson => {
+      setPersons(persons.concat(newPerson))
       setNewName('')
       setNewNumber('')
     })
